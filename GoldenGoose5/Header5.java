f8c337ae92a5a0938821a455290b3719186f612b
Anthony Bao <anthonybao1999@gmail.com>
2016-07-02 20:10:03 -0700

    Update Header5.java

--- GoldenGoose5/Header5.java
+++ GoldenGoose5/Header5.java
@@ -1,24 +1,18 @@
-/*    */ package GoldenGoose5;
-/*    */ 
-/*    */ import java.awt.Color;
-/*    */ import java.awt.Dimension;
-/*    */ import javax.swing.JPanel;
-/*    */ import javax.swing.JTextArea;
-/*    */ 
-/*    */ public class Header5 extends JPanel
-/*    */ {
-/* 11 */   static JTextArea texta = new JTextArea("Ready Player One");
-/*    */ 
-/*    */   public Header5() {
-/* 14 */     texta.setBackground(Color.YELLOW);
-/* 15 */     texta.setEditable(false);
-/* 16 */     setPreferredSize(new Dimension(ImageDrawer5.size.width, 30));
-/* 17 */     setBackground(Color.YELLOW);
-/* 18 */     add(texta);
-/*    */   }
-/*    */ }
+package GoldenGoose5;
 
-/* Location:           /Users/timcoker/Desktop/GoldenGoose.jar
- * Qualified Name:     GoldenGoose5.Header5
- * JD-Core Version:    0.6.0
- */
+import java.awt.Color;
+import java.awt.Dimension;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+
+public class Header5 extends JPanel {
+  static JTextArea texta = new JTextArea("Ready Player One");
+
+  public Header5() {
+    texta.setBackground(Color.YELLOW);
+    texta.setEditable(false);
+    setPreferredSize(new Dimension(ImageDrawer5.size.width, 30));
+    setBackground(Color.YELLOW);
+    add(texta);
+  }
+}
