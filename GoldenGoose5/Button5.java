75227893c7ca3c5035bc2606edf34eb481a1c814
Anthony Bao <anthonybao1999@gmail.com>
2016-07-02 20:09:35 -0700

    Update Button5.java

--- GoldenGoose5/Button5.java
+++ GoldenGoose5/Button5.java
@@ -1,45 +1,37 @@
-/*    */ package GoldenGoose5;
-/*    */ 
-/*    */ import java.awt.Color;
-/*    */ import java.awt.Font;
-/*    */ import java.awt.event.ActionEvent;
-/*    */ import java.awt.event.ActionListener;
-/*    */ import javax.swing.ImageIcon;
-/*    */ import javax.swing.JButton;
-/*    */ 
-/*    */ public class Button5 extends JButton
-/*    */   implements ActionListener
-/*    */ {
-/*    */   String text;
-/*    */   ImageIcon image;
-/*    */   String response;
-/* 17 */   Font font = new Font("Verdana", 1, 15);
-/* 18 */   static Color color = new Color(102, 204, 255);
-/*    */ 
-/*    */   public Button5(String txt, ImageIcon imgicon) {
-/* 21 */     super(txt, imgicon);
-/* 22 */     setVerticalTextPosition(0);
-/* 23 */     setHorizontalTextPosition(0);
-/* 24 */     this.text = txt;
-/* 25 */     this.image = imgicon;
-/* 26 */     this.response = "";
-/* 27 */     setFont(this.font);
-/* 28 */     setForeground(color);
-/* 29 */     addActionListener(this);
-/*    */   }
-/*    */ 
-/*    */   public void actionPerformed(ActionEvent a)
-/*    */   {
-/* 34 */     if (this.response == "poop")
-/* 35 */       setForeground(new Color(153, 51, 0));
-/*    */     else {
-/* 37 */       setForeground(Color.ORANGE);
-/*    */     }
-/* 39 */     setText(this.response);
-/*    */   }
-/*    */ }
+package GoldenGoose5;
 
-/* Location:           /Users/timcoker/Desktop/GoldenGoose.jar
- * Qualified Name:     GoldenGoose5.Button5
- * JD-Core Version:    0.6.0
- */
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+
+public class Button5 extends JButton implements ActionListener  {
+  String text;
+  ImageIcon image;
+  String response;
+  Font font = new Font("Verdana", 1, 15);
+  static Color color = new Color(102, 204, 255);
+
+  public Button5(String txt, ImageIcon imgicon) {
+    super(txt, imgicon);
+    setVerticalTextPosition(0);
+    setHorizontalTextPosition(0);
+    this.text = txt;
+    this.image = imgicon;
+    this.response = "";
+    setFont(this.font);
+    setForeground(color);
+    addActionListener(this);
+  }
+
+  public void actionPerformed(ActionEvent a)  {
+    if (this.response == "poop")
+      setForeground(new Color(153, 51, 0));
+    else {
+      setForeground(Color.ORANGE);
+    }
+    setText(this.response);
+  }
+}
