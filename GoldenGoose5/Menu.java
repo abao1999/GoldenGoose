3d09ec070e012bce661776ce18778a3cab207a3e
Anthony Bao <anthonybao1999@gmail.com>
2016-07-02 20:12:00 -0700

    Update Menu.java

--- GoldenGoose5/Menu.java
+++ GoldenGoose5/Menu.java
@@ -1,17 +1,10 @@
-/*    */ package GoldenGoose5;
-/*    */ 
-/*    */ import java.awt.Color;
-/*    */ import javax.swing.JPanel;
-/*    */ 
-/*    */ public class Menu extends JPanel
-/*    */ {
-/*    */   public Menu()
-/*    */   {
-/* 10 */     setBackground(Color.YELLOW);
-/*    */   }
-/*    */ }
+package GoldenGoose5;
 
-/* Location:           /Users/timcoker/Desktop/GoldenGoose.jar
- * Qualified Name:     GoldenGoose5.Menu
- * JD-Core Version:    0.6.0
- */
+import java.awt.Color;
+import javax.swing.JPanel;
+
+public class Menu extends JPanel  {
+  public Menu() {
+    setBackground(Color.YELLOW);
+  }
+}
